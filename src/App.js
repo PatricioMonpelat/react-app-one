dee6e97ae2002f761dfbd5f13e1a1db5c5af086a
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-25 20:44:03 -0300

    add state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Todo from './components/Todo';
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
 
 function App() {
   return (
@@ -9,8 +7,6 @@ function App() {
       <Todo text='param text 1'/>
       <Todo text='param text 2'/>
       <Todo text='param text 3'/>
-      <Modal/>
-      <Backdrop/>
     </div>
   );
 }
