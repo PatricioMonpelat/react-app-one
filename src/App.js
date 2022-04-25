27f4ce213ff33ada43484f21fbdf1d2d5044297a
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-25 18:06:26 -0300

    first component & css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import Todo from './components/Todo.js';
 
 function App() {
   return (
-  <div>Hello!</div>
+    <div>
+      <h1>My To Dos</h1>
+      <Todo text="param text 1"/>
+      <Todo text="param text 2"/>
+      <Todo text="param text 3"/>
+
+    </div>
   );
 }
 
