1664697c1ae846f9373e0561759e4dce5532d5a7
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-25 18:11:17 -0300

    delete button onClick function

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My To Dos</h1>
-      <Todo text="param text 1"/>
-      <Todo text="param text 2"/>
-      <Todo text="param text 3"/>
+      <Todo text='param text 1'/>
+      <Todo text='param text 2'/>
+      <Todo text='param text 3'/>
 
     </div>
   );
