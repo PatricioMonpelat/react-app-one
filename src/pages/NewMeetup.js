f862f398121ad92739b962ade6bbbc1df7720adb
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-26 17:04:19 -0300

    new meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,10 @@
-function NewMeetupPage(){
-    return <div> New Meetup page</div>
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  return <section> 
+      <h1>Add New Meetup</h1>
+    <NewMeetupForm/>
+  </section>;
 }
 
 export default NewMeetupPage;
