0c9c4e107e8e747787b80343dd4e5de4faf38382
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-04-25 23:57:56 -0300

    navigation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   // localhost:3000/
   // my-page.com/
-  //
 
   return (
     <div>
+      <MainNavigation/>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} exact />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
