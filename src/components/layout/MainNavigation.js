ad5f9474bc59a57868f85ca50702c4c920edcef4
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-10 12:02:26 -0300

    auth context

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,43 +1,63 @@
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import FavoritesContext from "../../store/favorites-context";
 import classes from "./MainNavigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 function MainNavigation() {
   const favortiesCtx = useContext(FavoritesContext);
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+
   const navigate = useNavigate();
 
-  function goHomeHandler(){
-    navigate('/');
-  };
+  function goHomeHandler() {
+    navigate("/");
+  }
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo} onClick={goHomeHandler}> Home</div>
+      <div className={classes.logo} onClick={goHomeHandler}>
+        Home{" "}
+      </div>
       <nav>
         <ul>
-          <li>
-            <Link to="/all-meetups">All Meetups</Link>
-          </li>
-          <li>
-            <Link to="/new-meetup">New Meetup</Link>
-          </li>
-          <li>
-            <Link to="/favorites">
-              My Favorites
-              <span className={classes.badge}>
-                {favortiesCtx.totalFavorites}
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/auth">Login</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-     
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/all-meetups">All Meetups</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/new-meetup">New Meetup</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/favorites">
+                My Favorites
+                <span className={classes.badge}>
+                  {favortiesCtx.totalFavorites}
+                </span>
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { FavoritesContextProvider } from "./store/favorites-context";
+import { AuthContextProvider } from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <FavoritesContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </FavoritesContextProvider>
+  <AuthContextProvider>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
+  </AuthContextProvider>
 );
