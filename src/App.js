b8045e406c857e49c54ebc8e7983a404f6a344fd
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-05-10 13:27:21 -0300

    token stored in localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import LoginPage from "./pages/LoginPage";
-import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
-import UserProfile from './components/Profile/UserProfile';
+import LoginPage from "./pages/LoginPage";
+
+import Layout from "./components/layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+
+import AuthContext from "./store/auth-context";
 
 function App() {
   // localhost:3000/
   // my-page.com/
+  const authCtx = useContext(AuthContext);
+
 
   return (
     <Layout>
-  
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/all-meetups" element={<AllMeetupsPage />} exact />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/auth" element={ <LoginPage />} />
-        <Route path="/profile" element={ <UserProfile />} />
+        {authCtx.isLoggedIn && (
+          <Route path="/all-meetups" element={<AllMeetupsPage />} exact />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/favorites" element={<FavoritesPage />} />
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/profile" element={<UserProfile />} />
+        )}
+        {!authCtx.isLoggedIn && <Route path="/auth" element={<LoginPage />} />}
+
+        <Route path='*'element={<HomePage />} />
+        
       </Routes>
     </Layout>
   );
